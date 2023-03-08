2565d3cc64a7e187db88578ded5f8fe471827e81
MarianoIsmael <110506444+MarianoIsmael@users.noreply.github.com>
2023-03-08 11:06:12 -0300

    Update autos.js

--- autos.js
+++ autos.js
@@ -1,1 +1,10 @@
-mkdir registro-automotor
+class Auto {
+  constructor(marca, modelo, anio, precio) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.anio = anio;
+    this.precio = precio;
+  }
+}
+
+module.exports = Auto;
